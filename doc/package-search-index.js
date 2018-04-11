bcedf0827fe9cac11dc313b879d6ea01aa1a16ba
YuKyeongPark <21600280@handong.edu>
2018-04-12 02:54:16 +0900

    First commit

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"edu.handong.csee.java.lab09.prob1"}]
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"edu.handong.csee.java.lab09.prob1","l":"SalesMan"},{"p":"edu.handong.csee.java.lab09.prob1","l":"SalesReporter"}]
